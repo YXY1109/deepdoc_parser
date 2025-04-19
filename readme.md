ecabc3fdff6310f9aac9a8db74418dabade800d0
yangxy <yangxy@jyiptv.com>
2025-04-19 10:11:49 +0800

    update：deepdoc的原始代码

---
+++ readme.md
@@ -0,0 +1,17 @@
+### uv命令
+
+> pip install uv 可选
+> 
+> uv init --python 3.11.11
+> 
+> source .venv/bin/activate
+> 
+> 如果有requirements.txt文件，执行以下命令，将添加到dependencies并安装
+> 
+> uv add -r requirements.txt
+>
+> 查看包版本：
+> pip install numpy==
+>
+> 直接添加
+> uv add numpy==1.26.4
---
+++ test_parser.py
@@ -0,0 +1,33 @@
+from parser import DocxParser, ExcelParser, TxtParser
+
+
+def test_docx_parser(file_path):
+    parser = DocxParser()
+    secs, tbls = parser(file_path)
+    print("DOCX 解析结果：")
+    print("段落内容：", secs)
+    print("表格内容：", tbls)
+
+
+def test_excel_parser(file_path):
+    parser = ExcelParser()
+    res = parser(file_path)
+    print("Excel 解析结果：")
+    print(res)
+
+
+def test_txt_parser(file_path):
+    parser = TxtParser()
+    result = parser(file_path)
+    print("TXT 解析结果：")
+    print(result)
+
+
+if __name__ == "__main__":
+    docx_file = "path/to/your/file.docx"
+    excel_file = "path/to/your/file.xlsx"
+    txt_file = "file/demo.txt"
+
+    # test_docx_parser(docx_file)
+    # test_excel_parser(excel_file)
+    test_txt_parser(txt_file)
